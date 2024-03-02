d522fd2a2102361e576832a23361f3003f3444d4
akshaya-1811 <akshayapulluru18@gmail.com>
2024-03-02 19:13:12 +0530

    created subnets

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,29 @@
+#VPC
 resource "aws_vpc" "my-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "akshaya"
+  }
+}
+
+#WEB-SUBNET
+resource "aws_subnet" "my-web-sn" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "web-sn"
+  }
+}
+
+#DATABSE-SUBNET
+resource "aws_subnet" "my-db-sn" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "db-sn"
   }
 }
