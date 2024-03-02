0249d34b9b55fe96cc6d346a98cfe2d07f15cd35
akshaya-1811 <akshayapulluru18@gmail.com>
2024-03-02 19:18:31 +0530

    enabled pip for web subnet

--- vpc.tf
+++ vpc.tf
@@ -12,6 +12,7 @@ resource "aws_vpc" "my-vpc" {
 resource "aws_subnet" "my-web-sn" {
   vpc_id     = aws_vpc.my-vpc.id
   cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch="true"
 
   tags = {
     Name = "web-sn"
